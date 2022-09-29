73a8af02c8365c43f800c00b09ee6d5e341fdfa6
LecsoBKW <LecsoBKW@users.noreply.github.com>
2022-09-29 14:31:13 +0200

    Update tag.sh

--- tag.sh
+++ tag.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 echo $1
+git config --global --add url."git@github.com:".insteadOf "https://github.com/"
 git config --global user.email "licsak.zoltan@gmail.com"
 git config --global user.name "LecsoBKW"
 git tag -a $1 -m $1
